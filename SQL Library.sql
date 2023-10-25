02d44d5e79d1ab55b5c79539c273dbe98c2043db
FaizalSriJamal <79001875+FaizalSriJamal@users.noreply.github.com>
2023-10-25 20:40:23 +0800

    Update SQL Library.sql

--- SQL Library.sql
+++ SQL Library.sql
@@ -108,46 +108,6 @@ WHERE Loans.DueDate= '2022-07-13'
 AND Loans.ReturnedDate IS NULL
 ;
 
--- Return three books to the library using
--- their bar codes.
--- Return date: July 5, 2022
--- Book 1: 6435968624
--- Book 2: 5677520613
--- Book 3: 8730298424
-
-
-UPDATE Loans
-SET ReturnedDate = '2022-07-05'
-WHERE BookID=
-(SELECT BookID
-FROM Books
-WHERE Barcode='6435968624')
-AND ReturnedDate IS NULL
-;
-
-
-UPDATE Loans
-SET ReturnedDate = '2022-07-05'
-WHERE BookID=
-(SELECT BookID
-FROM Books
-WHERE Barcode='5677520613')
-AND ReturnedDate IS NULL
-;
-
-UPDATE Loans
-SET ReturnedDate = '2022-07-05'
-WHERE BookID=
-(SELECT BookID
-FROM Books
-WHERE Barcode='8730298424')
-AND ReturnedDate IS NULL
-;
-
-SELECT * 
-FROM Loans
-WHERE ReturnedDate='2022-07-05'
-;
 
 -- Return three books to the library using
 -- their bar codes.
